f717049b09db3c1d3b6a0afbfde4366dc38bbf74
Hovhannes Galstyan <hovo21003@gmail.com>
2024-09-26 02:26:10 +0400

    Commit with a solution

---
+++ task.py
@@ -0,0 +1,20 @@
+from typing import Tuple, Optional
+from math import sqrt
+
+def solution(a, b, c) -> Optional[Tuple[float, float]]:
+
+    result = None
+
+    discriminant = b * b - 4 * a * c
+    if discriminant > 0:
+        result = (-b - sqrt(discriminant))/(2 * a), (-b + sqrt(discriminant))/(2 * a)
+    if discriminant == 0:
+        result = -b/(2 * a),
+    print (result)
+
+    return result
+
+if __name__ == "__main__":
+    assert solution(1, 6, 5) == (-5, -1)
+    assert solution(1, 4, 4) == (-2,)
+    assert solution(1, 6, 45) is None
